b6b31e90c3a239e4ea40bc1920b590a6cdb11f7f
Jorge <martaprietoluque@gmail.com>
2019-09-23 20:46:51 +0200

    Get id

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,7 +5,7 @@ const CharacterCard = props => {
   const { name, image, species } = props;
   return (
     <div className="box__name">
-      <div><img src={image} alt={name} /></div>
+      <div><img src={image} alt={`Foto de${name}`} /></div>
       <h2 className="title__name">{name}</h2>
       <h3 className="">{species}</h3>
     </div>
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import CharacterCard from './CharacterCard';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const CharacterList = props => {
@@ -12,7 +12,7 @@ const CharacterList = props => {
         .map(item => {
           return (
             <li className="list__characters" key={item.id}>
-              <Link to="/Char-detail" className="link__characters">
+              <Link to={`/Char-detail/${item.id}`} className="link__characters">
                 <CharacterCard
                   name={item.name}
                   image={item.image}
