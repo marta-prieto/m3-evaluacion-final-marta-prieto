c6c23a1c6c05fd673e915fcbc7e90d26b99e7573
Jorge <martaprietoluque@gmail.com>
2019-09-24 13:19:58 +0200

    Add build docs

---
+++ docs/precache-manifest.7e7e066f942b45f9c7d33dc8c66b2da6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1833efea03c2eafa890873283087f8fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "39e53570639b84765681",
+    "url": "./static/css/main.5d79299f.chunk.css"
+  },
+  {
+    "revision": "ed65b59d58d352534991",
+    "url": "./static/js/2.8ec95122.chunk.js"
+  },
+  {
+    "revision": "39e53570639b84765681",
+    "url": "./static/js/main.340d57ed.chunk.js"
+  },
+  {
+    "revision": "8faac2de49d54a9e0494",
+    "url": "./static/js/runtime-main.44d6403f.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logoRick.75509f2e.png"
+  }
+]);
